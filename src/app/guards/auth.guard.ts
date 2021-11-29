e0049e15767fb39404e2d47cfe30ee80f6611b20
Alexcordev <alexcordev@gmail.com>
2021-11-29 17:04:25 -0500

    Modifying auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,28 +1,23 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, Route, CanLoad } from '@angular/router';
-import { User } from '../interfaces/user';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, Route, CanLoad, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  user: User = { isUserAuthenticated: false, email: '', password: '' };
-  constructor(private router: Router) {
+
+  constructor(private router: Router, private authService: AuthService) {
   }
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-      return this.userIsAuthenticated();
-  }
-  canLoad(route: Route): boolean {
-      return this.userIsAuthenticated();
-  }
-  userIsAuthenticated() {
-      if (this.user.isUserAuthenticated) {
-          return true;
-      } else {
-          window.alert('Vous n\'avez pas les permissions pour voir cette page.');
-          this.router.navigateByUrl('');
-          return false;
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if (this.authService._userIsAuthenticated) {
+        return true;
       }
+      window.alert('Vous n\'avez pas la permission de voir cette page');
+      return false;
   }
 
 }
